ec50ae16b32293543497cd9b5d28ba8d515eb400
RyanHaniff <haniffryan.hr@gmail.com>
2023-08-09 14:23:51 -0400

    created module and service

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
+// imported the tast controller for us from `nest g controller tasks --no-spec`
 import { TasksController } from './tasks.controller';
+import { TasksService } from './tasks.service';
 
 @Module({
-  controllers: [TasksController]
+  controllers: [TasksController],
+  providers: [TasksService],
 })
 export class TasksModule {}
