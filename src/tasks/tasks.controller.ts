0abe719fd0a5619ca6558a4543c81b01dacfb86f
RyanHaniff <haniffryan.hr@gmail.com>
2023-08-09 17:03:13 -0400

    @Get module

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { TasksService } from './tasks.service';
 
 @Controller('tasks')
-export class TasksController {}
+export class TasksController {
+  // injected tasksservice inside our controller
+  constructor(private tasksService: TasksService) {}
+
+  // we want this method to be called when a Get request comes in
+  @Get()
+  getAllTasks() {
+    return this.getAllTasks();
+  }
+}
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  // set to private and allow methods to access this array
+  private tasks = [];
+
+  // public by defualt
+  getAllTasks() {
+    return this.tasks;
+  }
+}
