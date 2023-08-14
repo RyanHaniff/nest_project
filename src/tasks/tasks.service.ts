4227d6c7ccc0523255f9deb448cd97d86cbd0f48
RyanHaniff <haniffryan.hr@gmail.com>
2023-08-14 12:02:43 -0400

    created and returned the task

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -7,6 +7,7 @@ import { v4 as uuid } from "uuid";
 export class TasksService {
 	// set to private and allow methods to access this array
 	private tasks: Task[] = []; // type of task is a task array
+
 	// public by defualt
 	getAllTasks(): Task[] {
 		return this.tasks;
@@ -20,5 +21,10 @@ export class TasksService {
 			description,
 			status: TaskStatus.OPEN,
 		};
+    // push the task to the task array:
+    this.tasks.push(task);
+
+    // return the task, so the controller can return it in the http response
+    return task;
 	}
 }
