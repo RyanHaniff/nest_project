364b960fceb4ef433124f70546f0d7a1e0443db9
RyanHaniff <haniffryan.hr@gmail.com>
2023-08-17 13:55:12 -0400

    installed pipe packages and implemented @IsNotEmpty pipe

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // run the validation pipe here
+  // done before listening and making the server live
+  app.useGlobalPipes(new ValidationPipe());
+
   await app.listen(3000);
 }
 bootstrap();
--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,6 +1,14 @@
+import { IsNotEmpty } from "class-validator";
+
 export class CreateTaskDTO {
+
+    @IsNotEmpty() // used for validation to make sure the string are
+    // not empty
 	title: string;
+
+    @IsNotEmpty()
 	description: string;
     // if we add a new input here, we wont have to change any other code
     // we can change the desctructoring located in tasks.service.ts
 }
+
