f357a7eec1469d6f9b2ff9435a64cd08a769a9b9
RyanHaniff <haniffryan.hr@gmail.com>
2023-08-29 18:29:30 -0400

    Type ORM imports

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -2,8 +2,11 @@ import { Module } from '@nestjs/common';
 // imported the tast controller for us from `nest g controller tasks --no-spec`
 import { TasksController } from './tasks.controller';
 import { TasksService } from './tasks.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TasksRepository } from './tasks.repository';
 
 @Module({
+	imports: [TypeOrmModule.forFeature([TasksRepository])],
 	controllers: [TasksController],
 	providers: [TasksService],
 })
